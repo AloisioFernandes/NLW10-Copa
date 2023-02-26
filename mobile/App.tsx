d06d6eb74a06af340512be4bccc185f24e7e6216
AloisioFernandes <aloisio.fernandes@outlook.com>
2023-02-25 22:26:42 -0300

    Arquivo de tema.

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -6,23 +6,10 @@ import { NativeBaseProvider, Text, Center } from 'native-base'
 export default function App() {
   return (
     <NativeBaseProvider>
-      <Center flex={1} bgColor="fuchsia.400" alignItems="center" justifyContent="center">
-        <Text color="black" fontSize={24}>Hello React Native!</Text>
+      <Center flex={1} bgColor="black">
+        <Text color="white" fontSize={24}>Hello React Native!</Text>
         <StatusBar style="auto" />
       </Center>
     </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#000',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    color: '#FFF',
-    fontSize: 24,
-  }
-});
