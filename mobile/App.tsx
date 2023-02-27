1a655ab3d093bb0374feff15af8d915d96b90eb6
AloisioFernandes <aloisio.fernandes@outlook.com>
2023-02-27 18:35:41 -0300

    Utilizando tema native base.

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,13 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
-
 import { NativeBaseProvider, Text, Center } from 'native-base'
+
+import { THEME } from './src/styles/theme'
 
 export default function App() {
   return (
-    <NativeBaseProvider>
-      <Center flex={1} bgColor="black">
-        <Text color="white" fontSize={24}>Hello React Native!</Text>
+    <NativeBaseProvider theme={THEME}>
+      <Center flex={1} bgColor="gray.900">
+        <Text color="white" fontSize={24}>
+          Hello React Native!
+        </Text>
         <StatusBar style="auto" />
       </Center>
     </NativeBaseProvider>
