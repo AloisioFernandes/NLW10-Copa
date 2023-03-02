e79e4e01ac43b3ecbbd2c2f350736396e3b81bff
AloisioFernandes <aloisio.fernandes@outlook.com>
2023-03-02 19:21:12 -0300

    Status Bar customizada.

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,8 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import { NativeBaseProvider, Text, Center } from 'native-base'
+import { NativeBaseProvider, StatusBar } from 'native-base'
 import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto'
 
 import { Loading } from './src/components/Loading';
+import { SignIn } from './src/screens/SignIn';
 
 import { THEME } from './src/styles/theme'
 
@@ -11,15 +11,12 @@ export default function App() {
 
   return (
     <NativeBaseProvider theme={THEME}>
-      <Center flex={1} bgColor="gray.900">
-        {
-          !fontsLoaded &&  <Loading />
-        }
-        <Text color="white" fontSize={24}>
-          Hello React Native!
-        </Text>
-        <StatusBar style="auto" />
-      </Center>
+      <StatusBar 
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <SignIn /> : <Loading />}
     </NativeBaseProvider>
   );
 }
