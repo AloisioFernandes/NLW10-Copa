abe6e77e6fd054bb89db6af321a053359cef0f55
AloisioFernandes <aloisio.fernandes@outlook.com>
2023-03-11 19:35:34 -0300

    Implementando tela inicial.

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,20 +1,47 @@
-interface HomeProps {
-  count: number;
-}
+import Image from 'next/image'
+import appPreviewImage from '../assets/app-nlw-copa-preview.png'
+import logoImg from '../assets/logo.svg'
+import usersAvatarExampleImg from '../assets/users-avatar-example.png'
 
-export default function Home(props: HomeProps) {
+export default function Home() {
   return (
-    <h1>Contagem: {props.count}</h1>
+    <div>
+      <main>
+        <Image src={logoImg} alt="NLW Copa" />
+
+        <h1>Crie seu próprio bolão da copa e compartilhe entre amigos!</h1>
+
+        <div>
+          <Image src={usersAvatarExampleImg} alt="" />
+          <strong>
+            <span>+12.592</span> pessoas já estão usando
+          </strong>
+        </div>
+
+        <form>
+          <input type="text" required placeholder='Qual nome do seu bolão?' />
+          <button type='submit'>Criar meu bolão</button>
+        </form>
+
+        <p>Após criar seu bolão, você receberá um código único que poderá usar para convidar outras pessoas 🚀</p>
+      </main>
+
+      <Image 
+        src={appPreviewImage} 
+        alt="Dois celulares exibindo uma prévia da aplicação móvel do NLW Copa" 
+        quality={100}
+      />
+    </div>
   )
 }
 
-export const getServerSideProps = async () => {
-  const response = await fetch('http://localhost:3333/pools/count')
-  const data = await response.json()
+// export const getServerSideProps = async () => {
+//   const response = await fetch('http://localhost:3333/pools/count')
+//   const data = await response.json()
 
-  return {
-    props: {
-      count: data.count,
-    }
-  }
-}
+//   return {
+//     props: {
+//       count: data.count,
+//     }
+//   }
+// }
