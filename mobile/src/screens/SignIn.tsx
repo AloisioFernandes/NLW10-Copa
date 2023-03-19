f4ed45224489ce29b67b14afa0cbcb4dc4922521
AloisioFernandes <aloisio.fernandes@outlook.com>
2023-03-19 19:47:18 -0300

    Importação de elementos SVG.

--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -1,13 +1,13 @@
-import React from "react";
+import React from "react"
 
 import { Center, Text } from "native-base";
+
+import Logo from '../assets/logo.svg'
 
 export function SignIn() {
   return (
     <Center flex={1} bgColor="gray.900">
-      <Text color="white" fontSize={24}>
-        SignIn
-      </Text>
+      <Logo />
     </Center>
   )
 }
