e3482a3d830ecd82fe0de095bebb78b5f9694afa
AloisioFernandes <aloisio.fernandes@outlook.com>
2023-03-20 21:22:16 -0300

    Botão com ícone.

--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -1,13 +1,21 @@
 import React from "react"
+import { Fontisto } from '@expo/vector-icons'
 
-import { Center, Text } from "native-base";
+import { Center, Text, Icon } from "native-base";
+
+import { Button } from "../components/Button";
 
 import Logo from '../assets/logo.svg'
 
 export function SignIn() {
   return (
     <Center flex={1} bgColor="gray.900">
-      <Logo />
+      <Logo width={212} height={40} />
+
+      <Button
+        title="ENTRAR COM GOOGLE"
+        leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
+      />
     </Center>
   )
 }
