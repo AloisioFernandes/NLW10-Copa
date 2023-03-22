d04ac48511727a92128298843c81dfd66cbaf063
AloisioFernandes <aloisio.fernandes@outlook.com>
2023-03-21 21:39:56 -0300

    Propriedades botão native base.

--- mobile/src/components/Button.tsx
+++ mobile/src/components/Button.tsx
@@ -2,11 +2,20 @@ import { Button as ButtonNativeBase, Text, IButtonProps } from 'native-base'
 
 interface Props extends IButtonProps {
   title: string;
+  type?: 'PRIMARY' | 'SECONDARY';
 }
 
-export function Button({ title, ...rest }: Props) {
+export function Button({ title, type = 'PRIMARY', ...rest }: Props) {
   return (
-    <ButtonNativeBase {...rest}>
+    <ButtonNativeBase 
+      w="full"
+      h={14}
+      rounded="sm"
+      fontSize="md"
+      textTransform="uppercase"
+      bg={type === 'SECONDARY' ? 'red.500' : 'yellow.500'}
+      {...rest}
+    >
       <Text>
         {title}
       </Text>
