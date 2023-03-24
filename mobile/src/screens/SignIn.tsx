dcfe31baebe105f6b9028d27cb8b276fc01fd446
AloisioFernandes <aloisio.fernandes@outlook.com>
2023-03-23 21:49:39 -0300

    EStilização de texto.

--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -9,7 +9,7 @@ import Logo from '../assets/logo.svg'
 
 export function SignIn() {
   return (
-    <Center flex={1} bgColor="gray.900">
+    <Center flex={1} bgColor="gray.900" p={7}>
       <Logo width={212} height={40} />
 
       <Button
@@ -17,6 +17,11 @@ export function SignIn() {
         leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
         type="SECONDARY"
       />
+
+      <Text color="white" textAlign="center" mt={4}>
+        Não utilizamos nenhuma informação além {'\n'}
+        do seu e-mail para criação de sua conta.
+      </Text>
     </Center>
   )
 }
