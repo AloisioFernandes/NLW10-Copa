97a307819daad530ab8e9637ce0c3758220d9333
AloisioFernandes <aloisio.fernandes@outlook.com>
2023-03-25 19:31:23 -0300

    Contexto de autenticação.

--- mobile/src/context/AuthContext.tsx
+++ mobile/src/context/AuthContext.tsx
@@ -1,3 +1,31 @@
 import { createContext } from "react";
 
-export const AuthContext = createContext({})
+interface UserProps {
+  name: string;
+  avatarUrl: string;
+}
+
+export interface AuthContextDataProps {
+  user: UserProps;
+  signIn: () => Promise<void>;
+}
+
+export const AuthContext = createContext({} as AuthContextDataProps)
+
+export function AuthContextProvider({ children }){
+  async function signIn(){
+
+  }
+
+  return (
+    <AuthContext.Provider value={{
+      signIn,
+      user: {
+        name: 'Aloísio',
+        avatarUrl: 'https://github.com/AloisioFernandes.png'
+      }
+    }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
