0004f3e5185fb073b3cb4b9d571366fd3cf3ecba
AloisioFernandes <aloisio.fernandes@outlook.com>
2023-04-10 22:14:27 -0300

    Rota para criação de usuários.

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,6 +1,22 @@
 import { FastifyInstance } from "fastify"
+import { z } from "zod"
 import { prisma } from "../lib/prisma"
 
 export async function authRoutes(fastify: FastifyInstance) {
-  
+  fastify.post('/users', async (request) => {
+    const createUserBody = z.object({
+      access_token: z.string()
+    })
+
+    const { access_token } = createUserBody.parse(request.body)
+
+    const userReponse = await fetch('https://www.googleapis.com/oauth2/v2/userinfo', {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${access_token}`
+      }
+    })
+
+    const userData = await userReponse.json()
+  })
 }
