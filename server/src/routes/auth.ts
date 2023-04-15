45318152d55e99d8b5f27b232918058f380dbcff
AloisioFernandes <aloisio.fernandes@outlook.com>
2023-04-14 21:03:58 -0300

    Geração de jwt.

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,8 +1,15 @@
 import { FastifyInstance } from "fastify"
 import { z } from "zod"
 import { prisma } from "../lib/prisma"
+import { authenticate } from "../plugins/authenticate"
 
 export async function authRoutes(fastify: FastifyInstance) {
+  fastify.get('/me', {
+    onRequest: [authenticate],
+  }, async (request) => {
+    return { user: request.user }
+  })
+
   fastify.post('/users', async (request) => {
     const createUserBody = z.object({
       access_token: z.string()
@@ -34,7 +41,7 @@ export async function authRoutes(fastify: FastifyInstance) {
       }
     })
 
-    if (user) {
+    if (!user) {
       user = await prisma.user.create({
         data: {
           googleId: userInfo.id,
@@ -45,6 +52,14 @@ export async function authRoutes(fastify: FastifyInstance) {
       })
     }
 
-    return { userInfo }
+    const token = fastify.jwt.sign({
+      name: user.name,
+      avatarUrl: user.avatarUrl
+    }, {
+      sub: user.id,
+      expiresIn: '7 days',
+    })
+
+    return { token }
   })
 }
