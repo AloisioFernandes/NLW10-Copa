24e18f28867af1bac152064fdc3dad004acf384d
AloisioFernandes <aloisio.fernandes@outlook.com>
2023-04-17 19:40:46 -0300

    Listagem de bolões que participa.

--- server/src/routes/pool.ts
+++ server/src/routes/pool.ts
@@ -103,4 +103,45 @@ export async function poolRoutes(fastify: FastifyInstance) {
 
     return reply.status(201).send()
   })
+
+  fastify.get('/pools', {
+    onRequest: [authenticate]
+  }, async (request) => {
+    const pools = await prisma.pool.findMany({
+      where: {
+        participant: {
+          some: {
+            userId: request.user.sub,
+          }
+        }
+      },
+      include: {
+        _count: {
+          select: {
+            participant: true,
+          }
+        },
+        participant: {
+          select: {
+            id: true,
+
+            user: {
+              select: {
+                avatarUrl: true,
+              }
+            }
+          },
+          take: 4,
+        },
+        owner: {
+          select: {
+            id: true,
+            name: true,
+          }
+        },
+      }
+    })
+
+    return { pools }
+  })
 }
