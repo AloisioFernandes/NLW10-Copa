d7d45f29ba443e965279d3da0295e1bf1d9fb8e9
AloisioFernandes <aloisio.fernandes@outlook.com>
2023-04-20 23:09:21 -0300

    Rota games.

--- server/src/routes/game.ts
+++ server/src/routes/game.ts
@@ -1,6 +1,19 @@
 import { FastifyInstance } from "fastify"
+import { z } from 'zod'
 import { prisma } from "../lib/prisma"
+import { authenticate } from '../plugins/authenticate'
 
 export async function gameRoutes(fastify: FastifyInstance) {
+  fastify.get('/pools/:id/games', {
+    onRequest: [authenticate]
+  }, async (request) => {
+    const getGameParams = z.object({
+      id: z.string()
+    })
+
+    const { id } = getGameParams.parse(request.params)
+
+    const games = await prisma.game.findMany({})
+  })
   
 }
