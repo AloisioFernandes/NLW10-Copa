72c853c9d4587cea6b1af4d36d6dad79d4fa0fa8
AloisioFernandes <aloisio.fernandes@outlook.com>
2023-04-30 22:09:04 -0300

    Componente de rotas.

---
+++ mobile/src/routes/app.routes.tsx
@@ -0,0 +1,16 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { New } from '../screens/New'
+
+const { Navigator, Screen } = createBottomTabNavigator()
+
+export function AppRoutes() {
+  return (
+    <Navigator>
+      <Screen 
+        name='new'
+        component={New}
+      />
+    </Navigator>
+
+  )
+}
