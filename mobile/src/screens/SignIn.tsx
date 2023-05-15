2fac9a6e2687db20654b608ce746d74d6e36cbfb
AloisioFernandes <aloisio.fernandes@outlook.com>
2023-05-15 20:59:53 -0300

    Configuração de rotas.

--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -8,7 +8,7 @@ import { Button } from "../components/Button";
 import Logo from '../assets/logo.svg'
 
 export function SignIn() {
-  const { signIn } = useAuth()
+  const { signIn, isUserLoading } = useAuth()
 
   return (
     <Center flex={1} bgColor="gray.900" p={7}>
@@ -20,6 +20,7 @@ export function SignIn() {
         type="SECONDARY"
         mt={12}
         onPress={signIn}
+        isLoading={isUserLoading}
       />
 
       <Text color="white" textAlign="center" mt={4}>
