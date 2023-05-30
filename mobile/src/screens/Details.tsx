9242227e2cc257b073959042c894f710bdbdb6da
AloisioFernandes <aloisio.fernandes@outlook.com>
2023-05-29 22:26:20 -0300

    Navegação details.

--- mobile/src/screens/Details.tsx
+++ mobile/src/screens/Details.tsx
@@ -1,7 +1,11 @@
 import { VStack } from "native-base";
+import { useRoute } from '@react-navigation/native'
+
 import { Header } from "../components/Header";
 
 export function Details() {
+  const route = useRoute()
+
   return (
     <VStack flex={1} bgColor="gray.900">
       <Header title="Título do bolão" showBackButton showShareButton/>
@@ -9,3 +13,4 @@ export function Details() {
     </VStack>
   )
 }
+// 1:03:00
