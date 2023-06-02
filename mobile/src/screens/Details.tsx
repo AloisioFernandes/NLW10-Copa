97c25820b72d926ceee17c0155d89e21c338e71a
AloisioFernandes <aloisio.fernandes@outlook.com>
2023-06-02 20:04:12 -0300

    Buscando informações do bolão.

--- mobile/src/screens/Details.tsx
+++ mobile/src/screens/Details.tsx
@@ -1,15 +1,51 @@
-import { VStack } from "native-base";
+import { useEffect, useState } from "react"
+
+import { useToast, VStack } from "native-base";
 import { useRoute } from '@react-navigation/native'
 
+import { api } from "../services/api";
+
 import { Header } from "../components/Header";
+import { Loading } from "../components/Loading";
 
 interface RouteParams {
   id: string;
 }
 
 export function Details() {
+  const [isLoading, setIsLoading] = useState(true)
+
   const route = useRoute()
+  const toast = useToast()
   const { id } = route.params as RouteParams
+
+  async function fetchPoolDetails(){
+    try {
+      setIsLoading(true)
+
+      const response = await api.get(`/pools/${id}`)
+
+    } catch (error) {
+      console.log(error)
+
+      toast.show({
+        title: 'Não foi possível carregar os detalhes do bolão',
+        placement: 'top',
+        bgColor: 'red.500'
+      })
+
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    fetchPoolDetails()
+  }, [])
+
+  if (isLoading) {
+    return <Loading />
+  }
 
   return (
     <VStack flex={1} bgColor="gray.900">
@@ -18,4 +54,3 @@ export function Details() {
     </VStack>
   )
 }
-// 1:05:30
